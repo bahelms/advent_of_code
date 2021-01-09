7625127ee9ca56de771d31ef24a4485fa981f74e
Barrett Helms <barrett.helms@salesloft.com>
2021-01-09 17:25:16 -0500

    Day 18b - refactor

--- src/day18.rs
+++ src/day18.rs
@@ -1,20 +1,37 @@
 use std::fs;
 
-// enum Op {
-//     Mul(Box<Op>, Box<Op>),
-//     Add(Box<Op>, Box<Op>),
-//     Num(u32),
-// }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Token {
+    Int(char),
+    Op(char),
+    Eof,
+}
 
-// impl Op {
-//     fn exec(&self) -> u32 {
-//         match self {
-//             Self::Mul(lhs, rhs) => lhs.exec() * rhs.exec(),
-//             Self::Add(lhs, rhs) => lhs.exec() + rhs.exec(),
-//             Self::Num(n) => *n,
-//         }
-//     }
-// }
+struct Lexer {
+    tokens: Vec<Token>,
+}
+
+impl Lexer {
+    fn new(input: String) -> Lexer {
+        let mut tokens: Vec<Token> = input
+            .chars()
+            .filter(|&ch| ch != ' ')
+            .map(|ch| match ch {
+                '0'..='9' => Token::Int(ch),
+                _ => Token::Op(ch),
+            })
+            .collect();
+        tokens.reverse();
+        Lexer { tokens }
+    }
+
+    fn next(&mut self) -> Token {
+        self.tokens.pop().unwrap_or(Token::Eof)
+    }
+    fn peek(&mut self) -> Token {
+        self.tokens.last().copied().unwrap_or(Token::Eof)
+    }
+}
 
 pub fn execute() {
     part_one();
@@ -22,18 +39,13 @@ pub fn execute() {
 }
 
 fn part_one() {
-    let sum: u64 = get_input()
-        .iter()
-        .map(|line| {
-            let input = parse(line);
-            calculate(&input[..])
-        })
-        .sum();
+    let sum: u64 = get_input().iter().map(|line| calculate(line.clone())).sum();
     print!("Day 18 - A: {:?}", sum);
 }
 
 fn part_two() {
-    println!(" - B: {:?}", 0);
+    let sum: u64 = get_input().iter().map(|line| calculate(line.clone())).sum();
+    println!(" - B: {:?}", sum);
 }
 
 fn get_input() -> Vec<String> {
@@ -44,108 +56,86 @@ fn get_input() -> Vec<String> {
         .collect()
 }
 
-fn parse(input: &String) -> Vec<char> {
-    input.chars().filter(|&ch| ch != ' ').collect()
+fn calculate(input: String) -> u64 {
+    let mut lexer = Lexer::new(input);
+    evaluate(&mut lexer, 0)
 }
 
-fn calculate(chars: &[char]) -> u64 {
-    let mut lhs: u64 = 0;
-    let mut pointer = 0;
-
-    while pointer < chars.len() {
-        let mut seeker = pointer + 1;
-        let ch = chars[pointer];
-        match ch.to_digit(10) {
-            Some(num) => {
-                lhs = num as u64;
-                pointer += 1;
-            }
-            None => {
-                // find lhs
-                if ch == '(' {
-                    let mut groups = 1;
-                    while groups > 0 {
-                        match chars[seeker] {
-                            '(' => groups += 1,
-                            ')' => groups -= 1,
-                            _ => {}
-                        }
-                        seeker += 1;
-                    }
-                    lhs = calculate(&chars[pointer + 1..seeker - 1]);
-                    pointer = seeker;
-                    continue;
-                }
-
-                // find rhs
-                let next_char = chars[seeker];
-                let rhs = match next_char.to_digit(10) {
-                    Some(num) => num as u64,
-                    None => match next_char {
-                        '(' => {
-                            let mut groups = 1;
-                            while groups > 0 {
-                                seeker += 1;
-                                match chars[seeker] {
-                                    '(' => groups += 1,
-                                    ')' => groups -= 1,
-                                    _ => {}
-                                }
-                            }
-                            calculate(&chars[pointer + 2..seeker])
-                        }
-                        _ => panic!("no rhs next char match"),
-                    },
-                };
-
-                match ch {
-                    '+' => lhs += rhs,
-                    '*' => lhs *= rhs,
-                    _ => panic!("operator is wrong"),
-                }
-                pointer = seeker + 1;
-            }
+fn evaluate(lexer: &mut Lexer, precedence: u8) -> u64 {
+    let mut lhs = match lexer.next() {
+        Token::Int(ch) => ch.to_digit(10).unwrap() as u64,
+        Token::Op('(') => {
+            let lhs = evaluate(lexer, 0);
+            assert_eq!(lexer.next(), Token::Op(')'));
+            lhs
         }
+        t => panic!("bad token: {:?}", t),
+    };
+
+    loop {
+        let op = match lexer.peek() {
+            Token::Eof => break,
+            Token::Op(op) => op,
+            t => panic!("bad token: {:?}", t),
+        };
+
+        if let Some((lbp, rbp)) = infix_binding_power(op) {
+            if lbp < precedence {
+                break;
+            }
+            lexer.next();
+            let rhs = evaluate(lexer, rbp);
+            match op {
+                '+' => lhs += rhs,
+                '*' => lhs *= rhs,
+                _ => panic!("unknown operator: {}", op),
+            }
+            continue;
+        }
+        break;
     }
     lhs
 }
 
-// fn group_by_precedence(chars: Vec<char>) -> Vec<char> {}
+fn infix_binding_power(op: char) -> Option<(u8, u8)> {
+    match op {
+        '+' | '*' => Some((1, 2)),
+        // part two
+        // '*' => Some((1, 2)),
+        // '+' => Some((3, 4)),
+        _ => None,
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::{calculate, parse};
-
-    // #[test]
-    // fn group_by_precedence() {
-    //     let input = group_by_precedence(parse(&"2 * 3 + 4".to_string()));
-    //     assert_eq!(input, vec!['(', '2', '*', '(', '3', '+', '4', ')', ')']);
-    //     assert_eq!(calculate(&input[..]), 14);
-    // }
+    use super::calculate;
 
     #[test]
-    fn calculate_root_group() {
-        let input = parse(&"2 + 3 * 4".to_string());
-        assert_eq!(calculate(&input[..]), 20);
+    fn calculate_single_expression() {
+        assert_eq!(calculate("2".to_string()), 2);
     }
 
     #[test]
-    fn calculate_with_starting_nested_group() {
-        let input = vec!['(', '2', '*', '3', ')', '+', '(', '4', '*', '5', ')'];
-        assert_eq!(calculate(&input[..]), 26);
+    fn calculate_arith_expression() {
+        assert_eq!(calculate("2 + 5".to_string()), 7);
     }
 
     #[test]
-    fn calculate_with_many_nested_groups() {
-        let input = vec![
-            '2', '*', '3', '+', '(', '4', '*', '(', '5', '+', '8', ')', ')',
-        ];
-        assert_eq!(calculate(&input[..]), 58);
+    fn calculate_deep_arith_expression() {
+        assert_eq!(calculate("3 + 5 * 2 + 1".to_string()), 17);
     }
 
     #[test]
-    fn calculate_big_one() {
-        let input = parse(&"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string());
-        assert_eq!(calculate(&input[..]), 13632);
+    fn calculate_first_grouped_xpression() {
+        assert_eq!(calculate("(3 + 5) * 2".to_string()), 16);
+    }
+
+    #[test]
+    fn calculate_first_grouped_expression() {
+        assert_eq!(
+            calculate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string()),
+            13632
+        );
     }
 }
